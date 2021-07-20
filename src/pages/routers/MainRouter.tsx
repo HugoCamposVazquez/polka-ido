cb87a44079e8e876fa9dc967fad867a96b06fd6d
morrigan <belma.dzi@gmail.com>
2021-07-20 18:44:33 +0200

    Use eager connect

--- src/pages/routers/MainRouter.tsx
+++ src/pages/routers/MainRouter.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { useEagerConnect } from '../../hooks/web3/useEagerConnect';
 import { AdminRouter } from './AdminRouter';
 import { UserRouter } from './UserRouter';
 
 export const MainRouter = (): any => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const triedEager = useEagerConnect();
+  // TODO: use loading until tried eager is true
+
   return (
     <Switch>
       {<Route path="/admin" component={AdminRouter} />}
