33f2149a5d371a7baa33126fd5a64292897e50a5
morrigan <belma.dzi@gmail.com>
2021-07-20 18:00:31 +0200

    Move css to styled

--- src/shared/insets/user/WalletConnect.styles.ts
+++ src/shared/insets/user/WalletConnect.styles.ts
@@ -1,4 +1,4 @@
-import { sideColor5 } from '../../../utils/colorsUtil';
+import { sideColor3, sideColor4, sideColor5 } from '../../../utils/colorsUtil';
 import { styled } from '../../../utils/css';
 
 export const connectWalletContainerStyle = styled.cssStyle`
@@ -6,4 +6,28 @@ export const connectWalletContainerStyle = styled.cssStyle`
   display: flex;
   margin-left: 1.4375rem;
   cursor: pointer;
+`;
+
+export const balanceStyle = styled.cssStyle`
+  color: ${sideColor4};
+  padding: 0.4375rem 0.5rem;
+  font-weight: 700;
+  font-size: 0.75rem;
+  font-family: 'Titillium Web';
+`;
+
+export const addressContainerStyle = styled.cssStyle`
+  background-color: ${sideColor3};
+  margin: 0.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 5.25rem;
+`;
+
+export const addressStyle = styled.cssStyle`
+  padding: 0.1875rem 0.5rem;
+  font-family: 'Titillium Web';
+  font-size: 0.75rem;
+  font-weight: 700;
 `;
--- src/shared/insets/user/WalletConnect.tsx
+++ src/shared/insets/user/WalletConnect.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-import { sideColor3, sideColor4 } from '../../../utils/colorsUtil';
 import { cs } from '../../../utils/css';
 import { MainButton } from '../../gui/MainButton';
 import { openWalletModal } from '../../modals/modals';
@@ -31,34 +30,9 @@ export const WalletConnect = ({ isMobile }: WalletConnectProps) => {
           onClick={() => {
             openWalletModal(setWalletConnected);
           }}>
-          <div
-            style={{
-              color: sideColor4,
-              padding: '0.4375rem 0.5rem',
-              fontWeight: 700,
-              fontSize: '0.75rem',
-              fontFamily: 'Titillium Web',
-            }}>
-            0.004233 ETH
-          </div>
-          <div
-            style={{
-              backgroundColor: sideColor3,
-              margin: '0.25rem',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              maxWidth: '5.25rem',
-            }}>
-            <div
-              style={{
-                padding: '0.1875rem 0.5rem',
-                fontFamily: 'Titillium Web',
-                fontSize: '0.75rem',
-                fontWeight: 700,
-              }}>
-              0xF2C...x706
-            </div>
+          <div style={styles.balanceStyle}>0.004233 ETH</div>
+          <div style={styles.addressContainerStyle}>
+            <div style={styles.addressStyle}>0xF2C...x706</div>
           </div>
         </div>
       )}
