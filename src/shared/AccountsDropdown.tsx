8ef0c69b2d130df1331cb0df131d632e622badcd
morrigan <belma.dzi@gmail.com>
2021-07-30 19:08:59 +0200

    Style dropdown

--- src/shared/AccountsDropdown.tsx
+++ src/shared/AccountsDropdown.tsx
@@ -1,7 +1,13 @@
+import './style/dropdown.css';
+
 import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
-import { Button, Dropdown, Menu } from 'antd';
+import { Dropdown, Menu } from 'antd';
 import { MenuInfo } from 'rc-menu/lib/interface';
 import React from 'react';
+
+import userIcon from '../assets/user_icon.svg';
+import * as styles from './AccountsDropdown.styles';
+import { MainButton } from './gui/MainButton';
 
 interface IProps {
   options: InjectedAccountWithMeta[];
@@ -15,14 +21,22 @@ export const AccountsDropdown = ({ options }: IProps) => {
   const menu = (
     <Menu onClick={onOptionSelect}>
       {options.map((option) => (
-        <Menu.Item key={option.meta.name}>{option.meta.name}</Menu.Item>
+        <Menu.Item key={option.meta.name}>
+          <div style={styles.accountOptionsContainerStyle}>
+            <img src={userIcon} style={styles.iconStyle} />
+            <div>
+              <p>{option.meta.name}</p>
+              <p style={styles.addressStyle}>{option.address}</p>
+            </div>
+          </div>
+        </Menu.Item>
       ))}
     </Menu>
   );
 
   return (
     <Dropdown overlay={menu} placement="bottomCenter" arrow trigger={['click']}>
-      <Button>Selected account</Button>
+      <MainButton type="bordered" title="Selected account" onClick={() => {}} />
     </Dropdown>
   );
 };
