d5447bf4af520cb846301aaf2fba8520b68c217d
morrigan <belma.dzi@gmail.com>
2021-08-15 21:04:10 +0200

    Add error if no env vars

--- src/services/pinata.ts
+++ src/services/pinata.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+if (!process.env.REACT_APP_PINATA_API_KEY || !process.env.REACT_APP_PINATA_API_SECRET) {
+  throw new Error('Please setup pinata.cloud credentials to be able to upload to IPFS.');
+}
+
 axios.defaults.baseURL = 'https://api.pinata.cloud';
 axios.defaults.headers.common['pinata_api_key'] = process.env.REACT_APP_PINATA_API_KEY;
 axios.defaults.headers.common['pinata_secret_api_key'] = process.env.REACT_APP_PINATA_API_SECRET;
