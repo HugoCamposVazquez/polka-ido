f7ff70de58127216edfef094d9aa40bd9c4892a5
morrigan <belma.dzi@gmail.com>
2021-08-19 18:37:24 +0200

    Add some disabled button style

--- src/shared/gui/MainButton.tsx
+++ src/shared/gui/MainButton.tsx
@@ -1,8 +1,9 @@
 import { Button } from 'antd';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { cs } from '../../utils/css';
 import * as styles from './MainButton.styles';
+
 type IProps = {
   title: string;
   onClick: () => void;
@@ -12,11 +13,22 @@ type IProps = {
 };
 
 export const MainButton = ({ title, onClick, type, style, disabled }: IProps) => {
+  const getButtonStyle = useCallback(() => {
+    if (disabled) {
+      return styles.fillButtonDisabledStyle;
+    }
+
+    if (type === 'fill') {
+      return styles.fillButtonStyle;
+    }
+    return styles.borderedButtonStyle;
+  }, [type, disabled]);
+
+  const buttonStyle = getButtonStyle();
+
+  console.log('buttonStyle: ', buttonStyle);
   return (
-    <Button
-      onClick={onClick}
-      style={cs(type === 'fill' ? styles.fillButtonStyle : styles.borderedButtonStyle, style)}
-      disabled={disabled}>
+    <Button onClick={onClick} style={cs(buttonStyle, style)} disabled={disabled}>
       {title}
     </Button>
   );
