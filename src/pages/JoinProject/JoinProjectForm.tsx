bff39afe562a33875ebac7d7f0c4f2c03d21b163
mrhr8 <mrhrzina0@gmail.com>
2021-08-19 13:16:03 +0200

    removing unused imports

--- src/pages/JoinProject/JoinProjectForm.tsx
+++ src/pages/JoinProject/JoinProjectForm.tsx
@@ -1,5 +1,4 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useWeb3React } from '@web3-react/core';
 import { BigNumber, ethers } from 'ethers';
 import React, { useEffect, useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
