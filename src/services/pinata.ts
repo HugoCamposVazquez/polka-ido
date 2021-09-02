288fae489216090a4c61fa6ecdf09aec769e8357
mrhr8 <mrhrzina0@gmail.com>
2021-09-02 12:48:02 +0200

    removing comments from code

--- src/services/pinata.ts
+++ src/services/pinata.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// if (!process.env.REACT_APP_PINATA_API_KEY || !process.env.REACT_APP_PINATA_API_SECRET) {
-//   throw new Error('Please setup pinata.cloud credentials to be able to upload to IPFS.');
-// }
+if (!process.env.REACT_APP_PINATA_API_KEY || !process.env.REACT_APP_PINATA_API_SECRET) {
+  throw new Error('Please setup pinata.cloud credentials to be able to upload to IPFS.');
+}
 
 export const instance = axios.create({
   baseURL: 'https://api.pinata.cloud',
