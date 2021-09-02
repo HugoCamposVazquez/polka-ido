fee798db779eb53d9e1399792892f642f1a63657
mrhr8 <mrhrzina0@gmail.com>
2021-09-02 09:58:36 +0200

    removing some code

--- src/hooks/web3/contract/useGetAllocation.ts
+++ src/hooks/web3/contract/useGetAllocation.ts
@@ -10,7 +10,7 @@ export const useGetAllocations = (address: string): string | undefined => {
   useEffect(() => {
     const getAllAlocations = async (): Promise<void> => {
       const total = await saleContract?.totalDepositPerUser();
-      const formmatedTotalDeposits = total && ethers.utils.formatEther(Number(total)).toString();
+      const formmatedTotalDeposits = total && ethers.utils.formatEther(total);
       setTotalDeposit(formmatedTotalDeposits);
     };
     getAllAlocations();
