666901b1fb5c84bc94f90469c974af14215449a8
mrhr8 <mrhrzina0@gmail.com>
2021-09-10 13:07:46 +0200

    limit short descrioption to 200 characters

--- src/pages/Admin/ProjectForm.tsx
+++ src/pages/Admin/ProjectForm.tsx
@@ -269,6 +269,7 @@ export const ProjectForm = ({ loadingProjectData, project, isEdit }: IProps) => 
                 mode={'light'}
                 placeholder={'Short description text here'}
                 style={{ height: '6.25rem' }}
+                maxLength={200}
               />
             </div>
           </div>
