664c465f1a57d09d2a50e315d02ad2977eb68f61
morrigan <belma.dzi@gmail.com>
2021-09-14 13:47:47 +0200

    Remove hardcoded USD

--- src/pages/LaunchpadPage.tsx
+++ src/pages/LaunchpadPage.tsx
@@ -7,7 +7,7 @@ import { useProjects } from '../hooks/apollo/useProjects';
 import { Footer } from '../shared/insets/user/Footer';
 import { LoadingData } from '../shared/LoadingData';
 import { ProjectCard } from '../shared/ProjectCard';
-import { ProjectData } from '../types/ProjectType';
+import { SalesDto } from '../types/ProjectType';
 import { getCardDirection } from '../utils/cardDirectionUtil';
 import { sideColor3 } from '../utils/colorsUtil';
 import { useWindowDimensions } from '../utils/windowDimensionsUtil';
@@ -15,7 +15,7 @@ import * as styles from './LaunchpadPage.styles';
 
 export const LaunchpadPage = () => {
   const [shownProjects, setShownProjects] = useState<'upcoming' | 'joined' | 'featured' | undefined>('upcoming');
-  const [projects, setProjects] = useState<ProjectData[]>([]);
+  const [projects, setProjects] = useState<SalesDto[]>([]);
   const { width } = useWindowDimensions();
 
   const { data: platformsData } = usePlatformsStats();
@@ -23,7 +23,7 @@ export const LaunchpadPage = () => {
 
   const filterUpcoming = useCallback((): void => {
     setShownProjects('upcoming');
-    let upcomingProjects: ProjectData[] = [];
+    let upcomingProjects: SalesDto[] = [];
     projectsData?.sales.map((project) => {
       if (getUnixTime(new Date()) < +project.startDate) {
         upcomingProjects.push(project);
@@ -36,7 +36,7 @@ export const LaunchpadPage = () => {
 
   const onClickFilterFeatured = useCallback((): void => {
     setShownProjects('featured');
-    let featuredProjects: ProjectData[] = [];
+    let featuredProjects: SalesDto[] = [];
     projectsData?.sales.map((project) => {
       if (project.featured) {
         featuredProjects.push(project);
@@ -54,7 +54,7 @@ export const LaunchpadPage = () => {
 
   const onClickShowAllProjects = useCallback((): void => {
     setShownProjects(undefined);
-    const allProjects: ProjectData[] = [];
+    const allProjects: SalesDto[] = [];
     projectsData?.sales.map((project) => {
       allProjects.push(project);
       setProjects(allProjects);
@@ -141,7 +141,7 @@ export const LaunchpadPage = () => {
       </div>
       <div className={styles.projectsCardsContainerParentClassName}>
         <div className={styles.projectsCardsContainerClassName}>
-          {projects.map((project: ProjectData, index: number) => {
+          {projects.map((project, index: number) => {
             return <ProjectCard key={index} project={project} direction={getCardDirection(width, index)} />;
           })}
         </div>
