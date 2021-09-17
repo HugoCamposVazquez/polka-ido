8df6374a568d21fef5c7812b5a0e60c32ac621f7
mrhr8 <mrhrzina0@gmail.com>
2021-09-17 16:17:27 +0200

    replacing hardcoded data

--- src/shared/modals/WalletModal.tsx
+++ src/shared/modals/WalletModal.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 import copyAddress from '../../assets/copy_address.svg';
 import viewInExplorer from '../../assets/view_in_explorer.svg';
+import { styled } from '../../utils/css';
+import { notifySuccess } from '../../utils/notifications';
 import { Modal } from './Modal';
 import * as styles from './WalletModal.styles';
 
+const ETHERSCAN_URL = process.env.REACT_APP_ETHERSCAN_URL;
 interface IProps {
   closeModal: () => void;
   changeWallet: any;
+  account: string;
 }
-
-export const WalletModal = ({ closeModal, changeWallet }: IProps) => {
+export const WalletModal = ({ closeModal, changeWallet, account }: IProps) => {
   return (
     <Modal title="ACCOUNT" closeModal={closeModal}>
       <div style={styles.boxStyle}>
@@ -25,17 +28,28 @@ export const WalletModal = ({ closeModal, changeWallet }: IProps) => {
             CHANGE
           </div>
         </div>
-        <div style={styles.addressTextStyle}>0xcF2C...c706</div>
+        <div style={styles.addressTextStyle}>
+          {account.slice(0, 6)}...{account.slice(-4)}
+        </div>
 
         <div style={styles.btnsContainerStyle}>
           <div style={styles.copyAddressParentStyle}>
             <img src={copyAddress} />
-            <div style={styles.copyAddressBtnStyle}>Copy Address</div>
+            <div
+              onClick={() => {
+                navigator.clipboard.writeText(account);
+                notifySuccess('Copied to Clipboard', 1250);
+              }}
+              style={styles.copyAddressBtnStyle}>
+              Copy Address
+            </div>
           </div>
-          <div style={styles.viewInExplorerParentStyle}>
-            <img src={viewInExplorer} />
-            <div style={styles.viewInExplorerBtnStyle}>View in Explorer</div>
-          </div>
+          <a style={styles.link} href={`${ETHERSCAN_URL}/${account}`} target="_blank" rel="noreferrer">
+            <div style={styles.viewInExplorerParentStyle}>
+              <img src={viewInExplorer} />
+              <div style={styles.viewInExplorerBtnStyle}>View in Explorer </div>
+            </div>
+          </a>
         </div>
       </div>
     </Modal>
