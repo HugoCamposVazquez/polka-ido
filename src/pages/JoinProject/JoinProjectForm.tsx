ec7f2c800070ccca2ab4b534b4211adf6e4407b0
irubido <ivan.rubido@gmail.com>
2021-11-08 17:58:32 +0100

    git stash fix

--- src/pages/JoinProject/JoinProjectForm.tsx
+++ src/pages/JoinProject/JoinProjectForm.tsx
@@ -53,16 +53,19 @@ export const JoinProjectForm = () => {
     try {
       notifyTransactionConfirmation('Confirm Transaction...', 'buyingTokens');
       setIsTransactionInProgress(true);
-      await saleContract?.buyTokens({
+      const tx = await saleContract?.buyTokens({
         value: ethers.utils.parseEther(fromValue),
         gasLimit: 10000000,
       });
-
-      updateNotifySuccess(<div>Success! Thank you for joining</div>, 'buyingTokens', 10000);
-
-      methods.setValue('toValue', '');
-      methods.setValue('fromValue', '');
-      setIsTransactionInProgress(false);
+      if (tx) {
+        const contractReceipt = await tx.wait(1);
+        if (contractReceipt) {
+          updateNotifySuccess(<div>Success! Thank you for joining</div>, 'buyingTokens', 10000);
+          methods.setValue('toValue', '');
+          methods.setValue('fromValue', '');
+          setIsTransactionInProgress(false);
+        }
+      }
     } catch (e) {
       console.error(e.message);
       updateNotifyError('Transaction Cancelled', 'buyingTokens');
