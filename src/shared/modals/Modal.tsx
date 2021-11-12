fcac54100340961a6ef229b5f3ef9581521e6fba
Bernard <bero4net@gmail.com>
2021-11-12 11:41:05 +0100

    implement modal outside click handler

--- src/shared/modals/Modal.tsx
+++ src/shared/modals/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import closeIcon from '../../assets/close_icon.svg';
 import * as styles from './Modal.styles';
@@ -10,9 +10,26 @@ interface IProps {
 }
 
 export const Modal = ({ closeModal, children, title }: IProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      closeModal();
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, closeModal]);
+
   return (
     <div style={styles.modalContainerStyle}>
-      <div style={styles.topRightBottomLeftNotch} className={styles.modalStyle}>
+      <div style={styles.topRightBottomLeftNotch} className={styles.modalStyle} ref={ref}>
         <div style={styles.titleParentStyle}>
           <div style={styles.titleTextStyle}>{title}</div>
           <div style={styles.closeIconParentStyle}>
