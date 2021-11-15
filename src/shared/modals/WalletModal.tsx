8fb39a751d3bc8560271839e4b8e71548b791423
Bernard <bero4net@gmail.com>
2021-11-15 10:57:35 +0100

    improve displaying address on different screens

--- src/shared/modals/WalletModal.tsx
+++ src/shared/modals/WalletModal.tsx
@@ -5,6 +5,7 @@ import viewInExplorer from '../../assets/view_in_explorer.svg';
 import { notifySuccess } from '../../utils/notifications';
 import { Modal } from './Modal';
 import * as styles from './WalletModal.styles';
+import { useWindowDimensions } from '../../utils/windowDimensionsUtil';
 
 interface IProps {
   closeModal: () => void;
@@ -12,6 +13,15 @@ interface IProps {
 }
 export const WalletModal = ({ closeModal, account }: IProps) => {
   const address = new URL(`address/${account}`, process.env.REACT_APP_MOONRIVER_URL);
+  const { width } = useWindowDimensions();
+
+  const accountAddress = (() => {
+    if (width < 350) return `${account.slice(0, 9)}...${account.slice(-7)}`;
+    if (width < 400) return `${account.slice(0, 10)}...${account.slice(-10)}`;
+    if (width < 450) return `${account.slice(0, 15)}...${account.slice(-10)}`;
+    if (width < 550) return `${account.slice(0, 20)}...${account.slice(-15)}`;
+    return account;
+  })();
 
   return (
     <Modal title="ACCOUNT" closeModal={closeModal}>
@@ -19,9 +29,7 @@ export const WalletModal = ({ closeModal, account }: IProps) => {
         <div style={styles.changeWalletParentStyle}>
           <div style={styles.connectedTextStyle}>Connected with MetaMask</div>
         </div>
-        <div style={styles.addressTextStyle}>
-          {account.slice(0, 6)}...{account.slice(-4)}
-        </div>
+        <div style={styles.addressTextStyle}>{accountAddress}</div>
 
         <div style={styles.btnsContainerStyle}>
           <div style={styles.copyAddressParentStyle}>
